18c8d9bb7ee91a3f1de3807ce6eac8c14b5de8ab
amandaomiranda <amandaoliveirabcc@gmail.com>
2025-11-26 08:27:52 -0300

    primeiro

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public record Main() {
+    public class Main {
+        public static void main(String[] args) {
+            System.out.println("Controle de Versão com Git e GitHub!");
+        }
+    }
+
+}
